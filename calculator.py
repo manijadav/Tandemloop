1d18faa6646702858380f837e600be7453560011
manijadav <63098069+manijadav@users.noreply.github.com>
2020-04-25 15:40:39 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,6 @@
+a = input("Enter A value: ")
+b = input("Enter B value: ")
+print("Addition of A and B is: ", a+b)
+print("Subraction of A and B is: ", a-b)
+print("Multiplication of A and B is: ", a*b)
+print("Division of A and B is: ", a/b)
