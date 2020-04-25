9c99497b78f5178b727c8c263ba91e59510a49e5
manijadav <63098069+manijadav@users.noreply.github.com>
2020-04-25 16:08:40 +0530

    Delete calculator.py

--- calculator.py
+++
@@ -1,6 +0,0 @@
-a = float(input("Enter A value: "))
-b = float(input("Enter B value: "))
-print("Addition of A and B is: ", a+b)
-print("Subraction of A and B is: ", a-b)
-print("Multiplication of A and B is: ", a*b)
-print("Division of A and B is: ", a/b)
